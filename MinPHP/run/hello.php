db7d7db331a216f5fa372344bb1cd138257d7c52
DM <769245396@qq.com>
2017-07-03 10:03:50 +0800

    欢迎页

--- MinPHP/run/hello.php
+++ MinPHP/run/hello.php
@@ -8,27 +8,28 @@
     <div class="info" style="font-size:14px;">
         <span style="font-size:30px;" class="glyphicon glyphicon-grain" aria-hidden="true"></span> <span style="font-size:16px;">【SP云】接口<span style="font-size:12px;position:relative;top:-7px;"> v1</span></span><br>
         <pre class="info" style="margin:10px 34px 10px 34px">
-关于SDK?
+关于
 。。。。。。
     密！！！！
 
-    1、sign签名方式
+    0、接口请求说明
+        除特殊说明外,所有接口请求时都需附带sign time两个参数,否则请求不予通过,sign time获取方式如下.
+        (提示:为避免每次请求前都获取服务器时间,可将服务器时间与本地时间的差值存于本地,每次请求时的服务器时间则为:本地时间 + 差值)
+    1、sign 签名生成方式
         <span class="label label-default">
-            md5( 参数按键名升序连接成字符串 + 秘钥 + 服务器时间 )
+            sign = md5( 请求参数按键名升序连接成字符串 + 秘钥 + 服务器时间 )
         </span>
     2、秘钥
         <span class="label label-default">
-            #$%^&**^$$kdjji&*(*7790;fi
+            key = #$%^&**^$$kdjji&*(*7790;fi
         </span>
-    5、无需签名接口
+    3、服务器时间(此接口无需签名验证)
         <span class="label label-default">
-            base/get-server-time
+            time = base/get-server-time
         </span>
-    6、其他说明
         </pre>
     </div>
     <div style="font-size:12px;position:absolute;bottom:0;right:20px;height:20px;text-align:right;">
         路过花开 | qq : 769245396 | <a target="_blank" href="http://www.srun.com/">深澜软件</a>
     </div>
 </div>
-<!--欢迎页 end-->
